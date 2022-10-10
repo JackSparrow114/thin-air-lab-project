1204cc75751e32f88867275ae36ccfd9617e5a3f
Arpit Shukla <JackSparrow114@users.noreply.github.com>
2022-10-10 14:18:59 -0400

    fixed code for zero quantities
    fixed code for zero quantities

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,36 +1,32 @@
 const unboundedKnapsack = (values, quantities, n, discounts) => {
-  
+
     let combo = discounts.map((dis, index) => (quantities[index]===0)? 0:dis*values[index]);
-    let totalQuantity = quantities.reduce(
-    (previousValue, currentValue) => previousValue + currentValue,
-    0
-    );
-    let lookup = new Array(totalQuantity).fill(0);
-    console.log(totalQuantity, combo, lookup);  
+    let totalQuantity = quantities.reduce((previousValue, currentValue) => previousValue + currentValue,0);
+    let lookup = new Array(totalQuantity).fill(0);  
 
     for(let i = 0; i < totalQuantity; i++){
-        if(i<5){
+        if(i<n){
         lookup[i] = combo[i]*(i+1);
         } else {
         lookup[i] = getMinValue(i, lookup);
         }
     }
-    console.log(lookup);
+console.log(lookup);
 
-    return lookup[totalQuantity-1];
+return lookup[totalQuantity-1];
 }
   
-  const getMinValue = (i, lookup) => {
+const getMinValue = (i, lookup) => {
     let midValue = Math.ceil((i+1)/2);
     let minPrice = Infinity;
     for(let j=0;j<midValue;j++){
-      let currentPrice = lookup[i-1-j]+lookup[j];
-      if(currentPrice<minPrice){
-        minPrice = currentPrice;
-      }
+        let currentPrice = lookup[i-1-j]+lookup[j];
+        if(currentPrice<minPrice){
+            minPrice = currentPrice;
+        }
     }
     return minPrice;
-  };
+};
   
 exports.calculate = function (req, res) {
     let inputData = {
@@ -49,9 +45,13 @@ exports.calculate = function (req, res) {
     for (const shirt in inputData) {
         quantities.push(inputData[shirt]);
     }
-    console.log(quantities);
+    const newQuantities = quantities.filter(s => s !== 0);
+    const newValues = values.filter((v, index) => quantities[index] !== 0);
+    const totalZeros = quantities.length - newQuantities.length;
+    discounts.splice(discounts.length - 2, totalZeros);
+    console.log(newQuantities, totalZeros, newValues, discounts);
 
-    let result = unboundedKnapsack(values, quantities, quantities.length, discounts)
+    let result = unboundedKnapsack(newValues, newQuantities, newQuantities.length, discounts);
     resultStr = result.toString();
     //
     console.log(resultStr);
